0c01fcbef4351140092cb32631d70e110252b0df
Rutvik Jathar <rutvikjathar75@gmail.com>
2025-06-18 15:47:01 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,289 @@
+-<?php 
+ini_set('session.cache_limiter','public');
+session_cache_limiter(false);
+session_start();
+include("config.php");
+								
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Meta Tags -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="shortcut icon" href="images/logo.ico">
+
+<!--	Fonts
+	========================================================-->
+<link href="https://fonts.googleapis.com/css?family=Muli:400,400i,500,600,700&amp;display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
+
+<!--	Css Link
+	========================================================-->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-slider.css">
+<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="css/layerslider.css">
+<link rel="stylesheet" type="text/css" href="css/color.css" id="color-change">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/flaticon/flaticon.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<!--	Title
+	=========================================================-->
+<title>Ecommerce - Real Estate</title>
+</head>
+<body>
+
+
+<!--	Page Loader  -->
+
+<div id="page-wrapper">
+    <div class="row"> 
+        <!--	Header start  -->
+		<?php include("include/header.php");?>
+        <!--	Header end  -->
+		
+        <!--	Banner Start   -->
+        <div class="overlay-black w-100 slider-banner1 position-relative" style="background-image: url('images/banner/04.jpg'); background-size: cover; background-position: center center; background-repeat: no-repeat;">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center">
+                    <div class="col-lg-12">
+                        <div class="text-white">
+                            <h1 class="mb-4"><span class="text-primary">Find</span><br>
+                                Your dream house</h1>
+                            <form method="post" action="propertygrid.php">
+                                <div class="row">
+                                    <div class="col-md-6 col-lg-2">
+                                        <div class="form-group">
+                                            <select class="form-control" name="type">
+                                                <option value="">Select Type</option>
+												<option value="appartment">Appartment</option>
+												<option value="bunglow">Bunglow</option>
+												<option value="house">House</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 col-lg-2">
+                                        <div class="form-group">
+                                            <select class="form-control" name="stype">
+                                                <option value="">Select Status</option>
+												<option value="rent">Rent</option>
+												<option value="sale">Sale</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8 col-lg-6">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" name="city" placeholder="Enter City or Enter State" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 col-lg-2">
+                                        <div class="form-group">
+                                            <button type="submit" name="filter" class="btn btn-primary w-100">Search Property</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--	Banner End  -->
+        
+        <!--	Text Block One
+		======================================================-->
+        <div class="full-row bg-gray">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h2 class="text-secondary double-down-line text-center mb-5">What We Do</h2></div>
+                </div>
+                <div class="text-box-one">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6">
+                            <div class="p-4 text-center hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+								<i class="flaticon-rent text-primary flat-medium" aria-hidden="true"></i>
+                                <h5 class="text-secondary hover-text-primary py-3 m-0"><a href="#">Selling Service</a></h5>
+                                <p>We are represent either party and help them achieve a purchase or sale with the best possible terms.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="p-4 text-center hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+								<i class="flaticon-for-rent text-primary flat-medium" aria-hidden="true"></i>
+                                <h5 class="text-secondary hover-text-primary py-3 m-0"><a href="#">Rental Service</a></h5>
+                                <p>Showing best property for user in their bugget.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="p-4 text-center hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+								<i class="flaticon-list text-primary flat-medium" aria-hidden="true"></i>
+                                <h5 class="text-secondary hover-text-primary py-3 m-0"><a href="#">Property Listing</a></h5>
+                                <p>Properties list by users requirements.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="p-4 text-center hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+								<i class="flaticon-diagram text-primary flat-medium" aria-hidden="true"></i>
+                                <h5 class="text-secondary hover-text-primary py-3 m-0"><a href="#">Legal Investment</a></h5>
+                                <p>Consult a financial advisor who will be able to guide you on the basis of your financial portfolio and help you build it further.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+		<!-----  Our Services  ---->
+		
+   
+        
+        <!--	Why Choose Us -->
+        <div class="full-row living bg-one overlay-secondary-half" style="background-image: url('images/back1.jpg'); background-size: cover; background-position: center center; background-repeat: no-repeat;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 col-lg-6">
+                        <div class="living-list pr-4">
+                            <h3 class="pb-4 mb-3 text-white">Why Choose Us</h3>
+                            <ul>
+                                <li class="mb-4 text-white d-flex"> 
+									<i class="flaticon-reward flat-medium float-left d-table mr-4 text-primary" aria-hidden="true"></i>
+									<div class="pl-2">
+										<h5 class="mb-3">Experience Quality</h5>
+										<p>Our knowledge, experience, expertise and resources set us apart allowing us to achieve results that don’t just meet, but exceed our clients’ expectations. It is our commitment to provide you with exceptional service from a team of highly skilled, uniquely talented individuals with high integrity, extensive professional knowledge and experience, far‐reaching connections and excellence in home marketing.</p>
+									</div>
+                                </li>
+                                <li class="mb-4 text-white d-flex"> 
+									<i class="flaticon-real-estate flat-medium float-left d-table mr-4 text-primary" aria-hidden="true"></i>
+									<div class="pl-2">
+										<h5 class="mb-3">Buying Strategies</h5>
+										<p>We use modern technology along with our market knowledge, unsurpassed negotiation and sales skills, professional experience and collaboration to assist you in identifying your desired property and securing the best price and terms, while protecting your interests..</p>
+									</div>
+                                </li>
+                                <li class="mb-4 text-white d-flex"> 
+									<i class="flaticon-seller flat-medium float-left d-table mr-4 text-primary" aria-hidden="true"></i>
+									<div class="pl-2">
+										<h5 class="mb-3">Communication And Responsiveness</h5>
+										<p>We are known for our proven ability to maintain open communication around the clock and a high level of responsiveness to our clients throughout the transaction process and beyond..</p>
+									</div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+		<!--	why choose us -->
+		
+	
+        
+        <!--	Achievement
+        ============================================================-->
+        <div class="full-row overlay-secondary" style="background-image: url('images/counterbg.jpg'); background-size: cover; background-position: center center; background-repeat: no-repeat;">
+            <div class="container">
+                <div class="fact-counter">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="count wow text-center  mb-sm-50" data-wow-duration="300ms"> <i class="flaticon-house flat-large text-white" aria-hidden="true"></i>
+								<?php
+										$query=mysqli_query($con,"SELECT count(pid) FROM property");
+											while($row=mysqli_fetch_array($query))
+												{
+										?>
+                                <div class="count-num text-primary my-4" data-speed="3000" data-stop="<?php 
+												$total = $row[0];
+												echo $total;?>">0</div>
+								<?php } ?>
+                                <div class="text-white h5">Property Available</div>
+                            </div>
+                        </div>
+						<div class="col-md-3">
+                            <div class="count wow text-center  mb-sm-50" data-wow-duration="300ms"> <i class="flaticon-house flat-large text-white" aria-hidden="true"></i>
+								<?php
+										$query=mysqli_query($con,"SELECT count(pid) FROM property where stype='sale'");
+											while($row=mysqli_fetch_array($query))
+												{
+										?>
+                                <div class="count-num text-primary my-4" data-speed="3000" data-stop="<?php 
+												$total = $row[0];
+												echo $total;?>">0</div>
+								<?php } ?>
+                                <div class="text-white h5">Sale Property Available</div>
+                            </div>
+                        </div>
+						<div class="col-md-3">
+                            <div class="count wow text-center  mb-sm-50" data-wow-duration="300ms"> <i class="flaticon-house flat-large text-white" aria-hidden="true"></i>
+								<?php
+										$query=mysqli_query($con,"SELECT count(pid) FROM property where stype='rent'");
+											while($row=mysqli_fetch_array($query))
+												{
+										?>
+                                <div class="count-num text-primary my-4" data-speed="3000" data-stop="<?php 
+												$total = $row[0];
+												echo $total;?>">0</div>
+								<?php } ?>
+                                <div class="text-white h5">Rent Property Available</div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="count wow text-center  mb-sm-50" data-wow-duration="300ms"> <i class="flaticon-man flat-large text-white" aria-hidden="true"></i>
+                                <?php
+										$query=mysqli_query($con,"SELECT count(uid) FROM user");
+											while($row=mysqli_fetch_array($query))
+												{
+										?>
+                                <div class="count-num text-primary my-4" data-speed="3000" data-stop="<?php 
+												$total = $row[0];
+												echo $total;?>">0</div>
+								<?php } ?>
+                                <div class="text-white h5">Registered Users</div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+		
+		
+        <!--	Footer   start-->
+		<?php include("include/footer.php");?>
+		<!--	Footer   start-->
+        
+        
+        <!-- Scroll to top --> 
+        <a href="#" class="bg-primary text-white hover-text-secondary" id="scroll"><i class="fas fa-angle-up"></i></a> 
+        <!-- End Scroll To top --> 
+    </div>
+</div>
+<!-- Wrapper End --> 
+
+<!--	Js Link
+============================================================--> 
+<script src="js/jquery.min.js"></script> 
+<!--jQuery Layer Slider --> 
+<script src="js/greensock.js"></script> 
+<script src="js/layerslider.transitions.js"></script> 
+<script src="js/layerslider.kreaturamedia.jquery.js"></script> 
+<!--jQuery Layer Slider --> 
+<script src="js/popper.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/owl.carousel.min.js"></script> 
+<script src="js/tmpl.js"></script> 
+<script src="js/jquery.dependClass-0.1.js"></script> 
+<script src="js/draggable-0.1.js"></script> 
+<script src="js/jquery.slider.js"></script> 
+<script src="js/wow.js"></script> 
+<script src="js/YouTubePopUp.jquery.js"></script> 
+<script src="js/validate.js"></script> 
+<script src="js/jquery.cookie.js"></script> 
+<script src="js/custom.js"></script>
+</body>
+
+</html>
---
+++ propertygrid.php
@@ -0,0 +1,191 @@
+<?php 
+ini_set('session.cache_limiter','public');
+session_cache_limiter(false);
+session_start();
+include("config.php");
+
+///search code
+	
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Meta Tags -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="ssvps">
+<meta name="keywords" content="">
+<meta name="author" content="Unicoder">
+<link rel="shortcut icon" href="images/logo.ico">
+
+<!--	Fonts
+	========================================================-->
+<link href="https://fonts.googleapis.com/css?family=Muli:400,400i,500,600,700&amp;display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
+
+<!--	Css Link
+	========================================================-->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-slider.css">
+<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="css/layerslider.css">
+<link rel="stylesheet" type="text/css" href="css/color.css" id="color-change">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/flaticon/flaticon.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<!--	Title
+	=========================================================-->
+<title>Ecommerce - Real Estate</title>
+</head>
+<body>
+
+ 
+
+
+<div id="page-wrapper">
+    <div class="row"> 
+        <!--	Header start  -->
+		<?php include("include/header.php");?>
+        <!--	Header end  -->
+        
+        <!--	Banner   --->
+        <div class="banner-full-row page-banner" style="background-image:url('images/breadcromb.jpg');">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2 class="page-name float-left text-white text-uppercase mt-1 mb-0"><b>Property Grid</b></h2>
+                    </div>
+                    <div class="col-md-6">
+                        <nav aria-label="breadcrumb" class="float-left float-md-right">
+                            <ol class="breadcrumb bg-transparent m-0 p-0">
+                                <li class="breadcrumb-item text-white"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">Property Grid</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+         <!--	Banner   --->
+        
+        <!--	Property Grid
+		===============================================================-->
+        <div class="full-row">
+            <div class="container">
+                <div class="row">
+				
+					<div class="col-lg-8">
+                        <div class="row">
+						
+							<?php 
+							
+							if(isset($_REQUEST['filter']))
+							{
+								$type=$_REQUEST['type'];
+								$stype=$_REQUEST['stype'];
+								$city=$_REQUEST['city'];
+								
+								$sql="SELECT * FROM property WHERE type='{$type}' and stype='{$stype}' and city='{$city}'";
+								//SELECT * FROM `property` WHERE type='office' or type='office' and stype='sale' or stype='rent' and city='valsad' OR state='mumbai'
+								//SELECT * FROM `property` WHERE type='office' and stype='sale'  and city='valsad' OR state='mumbai'
+								$result=mysqli_query($con,$sql);
+							
+								if(mysqli_num_rows($result)>0)
+								{
+									if($result == true)
+									{
+										while($row=mysqli_fetch_array($result))
+										{
+							?>
+									
+                            <div class="col-md-6">
+                                <div class="featured-thumb hover-zoomer mb-4">
+                                    <div class="overlay-black overflow-hidden position-relative"> <img src="images/property/<?php echo $row['18'];?>" alt="pimage">
+                                        
+                                        <div class="sale bg-secondary text-white">For <?php echo $row['5'];?></div>
+                                        <div class="price text-primary text-capitalize">₹<?php echo $row['13'];?> <span class="text-white"><?php echo $row['12'];?> Sqft</span></div>
+                                        
+                                    </div>
+                                    <div class="featured-thumb-data shadow-one">
+                                        <div class="p-4">
+                                            <h5 class="text-secondary hover-text-primary mb-2 text-capitalize"><a href="propertydetail.php?pid=<?php echo $row['0'];?>"><?php echo $row['1'];?></a></h5>
+                                            <span class="location text-capitalize"><i class="fas fa-map-marker-alt text-primary"></i> <?php echo $row['14'];?></span> </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                            <?php 		
+										} 
+					
+									}
+								}
+								else {
+									
+									echo "<h1 class='mb-5'><center>No Property Available</center></h1>";
+								}
+									
+							}
+
+							?>
+                            
+
+                            
+                    
+                        </div>
+                    </div>
+							
+								<?php 
+								$query=mysqli_query($con,"SELECT * FROM `property` ORDER BY date DESC LIMIT 2");
+										while($row=mysqli_fetch_array($query))
+										{
+								?>
+                                
+                                <?php } ?>
+
+                            </ul>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+
+        
+        <!--	Footer   start-->
+		<?php include("include/footer.php");?>
+		<!--	Footer   start-->
+        
+        <!-- Scroll to top --> 
+        <a href="#" class="bg-secondary text-white hover-text-secondary" id="scroll"><i class="fas fa-angle-up"></i></a> 
+        <!-- End Scroll To top --> 
+    </div>
+</div>
+<!-- Wrapper End --> 
+
+<!--	Js Link
+============================================================--> 
+<script src="js/jquery.min.js"></script> 
+<!--jQuery Layer Slider --> 
+<script src="js/greensock.js"></script> 
+<script src="js/layerslider.transitions.js"></script> 
+<script src="js/layerslider.kreaturamedia.jquery.js"></script> 
+<!--jQuery Layer Slider --> 
+<script src="js/popper.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/owl.carousel.min.js"></script> 
+<script src="js/tmpl.js"></script> 
+<script src="js/jquery.dependClass-0.1.js"></script> 
+<script src="js/draggable-0.1.js"></script> 
+<script src="js/jquery.slider.js"></script> 
+<script src="js/wow.js"></script> 
+
+<script src="js/custom.js"></script>
+</body>
+
+</html>
